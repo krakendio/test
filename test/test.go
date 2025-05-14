ba184844d19352e5c30231400fc4307db031319b
krakendio <yunus.aydin@trendyol.com>
2025-05-14 14:00:47 +0300

    Create test.go

---
+++ test/test.go
@@ -0,0 +1,7 @@
+// test/test.go
+package test
+
+// TestFunction is a simple function to return a greeting.
+func TestFunction() string {
+    return "Hello from github.com/krakendio/test! - hacked"
+}
